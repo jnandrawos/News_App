8c385f944d0fdf71af4c0520c5cfd6912bf80fd8
Jean Andrawos <jnandrawos@gmail.com>
2022-08-17 17:42:34 +0300

    Followed Naming Convention

--- app/src/main/java/com/example/newsapp/data/repository/EmailPreference.kt
+++ app/src/main/java/com/example/newsapp/data/repository/EmailPreference.kt
@@ -1,35 +1,25 @@
 package com.example.newsapp.data.repository
 
 import android.content.Context
-import android.content.SharedPreferences
-import androidx.security.crypto.EncryptedSharedPreferences
-import androidx.security.crypto.MasterKeys
 import com.example.newsapp.util.Constants
 
 class EmailPreference(context: Context) {
 
-    val masterKeyAlias = MasterKeys.getOrCreate(MasterKeys.AES256_GCM_SPEC)
-    val preference: SharedPreferences = EncryptedSharedPreferences.create(
-        Constants.shared_preferences_name,
-        masterKeyAlias,
-        context,
-        EncryptedSharedPreferences.PrefKeyEncryptionScheme.AES256_SIV,
-        EncryptedSharedPreferences.PrefValueEncryptionScheme.AES256_GCM)
-
+    val preference = context.getSharedPreferences(Constants.shared_preferences_name, Context.MODE_PRIVATE)
 
     fun getLoggedInEmail(): String? {
         return preference.getString(Constants.email_preference_name, null)
     }
 
-    fun setLoggedInEmail(email: String) {
+    fun setLoggedInEmail(email: String){
         val editor = preference.edit()
-        editor.putString(Constants.email_preference_name, email)
+        editor.putString(Constants.email_preference_name,email)
         editor.apply()
     }
 
-    fun clearLoggedInEmail() {
+    fun clearLoggedInEmail(){
         val editor = preference.edit()
-        editor.putString(Constants.email_preference_name, null)
+        editor.putString(Constants.email_preference_name,null)
         editor.apply()
     }
 
